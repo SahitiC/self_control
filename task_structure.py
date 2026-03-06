7ccdf2b1e52930613628f332b8ed52581f91945d
SahitiC <csahiti07@gmail.com>
2026-03-06 17:32:20 +0100

    modify transitions to include probability of success of resist and also modify reward functions to include rewards only on succeeding

--- task_structure.py
+++ task_structure.py
@@ -64,29 +64,33 @@ def rewards_cake(states, reward_tempt, effort_resist,
     # get reward matrix
     reward_func = []
     # reward for state 0, for tempt, resist
-    # if tempt -> r_tempt and if resist -> e_resist
-    # (doesn't matter what the next state is ):
-    reward_func.append([np.full(len(states), reward_tempt),
-                        np.full(len(states), effort_resist)])
+    # if tempt -> r_tempt and if resist -> e_resist only if resist works
+    # and r_tempt if resist doesn't work
+    reward_func.append([np.array([reward_tempt, 0]),
+                        np.array([reward_tempt, effort_resist])])
     # reward for state 1, for tempt, resist
-    # (if tempt -> r_tempt & if resist -> e_resist) + r_resist from last
-    # timestep in both cases (doesn't matter what the next state is)
-    reward_func.append([np.full(len(states), reward_resist + reward_tempt),
-                        np.full(len(states), reward_resist + effort_resist)])
+    # (if tempt -> r_tempt & if resist -> e_resist if resist works, r_tempt if
+    # resist doesnt work) + r_resist from last step in both cases
+    reward_func.append([np.array([reward_resist + reward_tempt, 0]),
+                        np.array([reward_resist + reward_tempt,
+                                  reward_resist + effort_resist])])
     reward_func_last = np.array([0.0, reward_resist])
 
     return reward_func, reward_func_last
 
 
-def transitions_cake():
+def transitions_cake(p=1.0):
+
+    # p is the probability that resisting will actually lead to resisting
+    # (and hence transition to state 1), when it fails, remain in state 0
 
     # get transition function
     T = []
-    # transition for state 0, if tempt -> state 0, if resist -> state 1
+    # for state 0, if tempt -> state 0, if resist -> state 1 w prob p
     T.append([np.array([1, 0]),
-              np.array([0, 1])])
-    # transition for state 1, if tempt -> state 0, if resist -> state 1
+              np.array([1-p, p])])
+    # for state 1, if tempt -> state 0, if resist -> state 1 w prob p
     T.append([np.array([1, 0]),
-              np.array([0, 1])])
+              np.array([1-p, p])])
 
     return T
